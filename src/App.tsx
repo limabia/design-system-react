1b1f69eb44114cd566884a28fc7bffed395cec19
limabia <bianca.lima.1504@gmail.com>
2022-10-14 19:09:39 -0300

    Start project react using tailwind, vite and postcss

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./styles/global.css";
+
+export function App() {
+  return (
+    <>
+      <h1 className="font-bold text-9xl text-green-400">Hello world</h1>
+
+      <button className="bg-violet-500 font-medium px-4 py-2 rounded text-white hover:bg-violet-600">
+        Enviar
+      </button>
+    </>
+  );
+}
